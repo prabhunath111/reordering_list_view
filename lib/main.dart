6dcb1bc4c4d12188ac904dbaa118d1fa98aaf9c7
prabhu_nath <prabhu_nath.tiwary@aponiar.com>
2020-02-06 19:31:38 +0530

    added dismissible and reordering

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
   List<String> data = ['aaa','bbb','ccc','ddd','eee','fff','ggg','hhh','iii','jjj'];
+  final items = List<String> .generate(20,(i)=>"Item ${i + 1}");
 
   void onreorder(int oldIndex, int newIndex){
 
@@ -19,8 +21,8 @@ class _HomePageState extends State<HomePage> {
         newIndex-=1;
       }
 
-      var x= data.removeAt(oldIndex);
-      data.insert(newIndex, x);
+      var x= items.removeAt(oldIndex);
+      items.insert(newIndex, x);
     });
 
   }
@@ -32,10 +34,35 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Center(
         child: ReorderableListView(
-            children: data.map((index)=>ListTile(key: ObjectKey(index),title: Text('${index}'),subtitle: Text('Move it anywhere'),)).toList(),
+            children:
+            items.map((index){
+              return Dismissible(
+                child: ListTile(key: ObjectKey(index),title: Text('${index}'),subtitle: Text('Move it anywhere'),
+                ),
+                  direction: DismissDirection.endToStart,
+                  background: Container(color: Colors.red),
+                  secondaryBackground: _secondBackground(),
+                  movementDuration: Duration(seconds: 2),
+                  key: Key(index),
+
+              );
+            }).toList(),
+
             onReorder: onreorder,
         ),
       ),
+    );
+  }
+
+  _secondBackground() {
+    return Container(
+      margin: EdgeInsets.all(12.0),
+      alignment: Alignment.centerRight,
+      decoration: BoxDecoration(
+        color: Colors.red,
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Icon(Icons.delete),
     );
   }
 }
